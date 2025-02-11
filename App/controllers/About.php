5d0ef8c5644b23dc9c2dd7f6fecbb96b69ecfc1d
Cloud White Knight <muhammadzakywijaya01@gmail.com>
2025-02-11 10:59:22 +0800

    Update About.php

--- App/controllers/About.php
+++ App/controllers/About.php
@@ -1,14 +1,24 @@
 <?php
 
-class About {
-  public function index($nama = 'zaky fakhri', $status = 'pelajar')
-  {
-    echo "Halo, nama saya $nama, saya adalah $status";
-  }
-  public function page()
-  {
-    echo 'About/page';
-  }
+class About extends Controller {
+   public function index($nama = 'fakhri', $status = 'Murid', $umur = 15)
+   {
+      $data['nama'] = $nama;
+      $data['status'] = $status; 
+      $data['umur'] = $umur; 
+      $data['judul'] = 'About Me';
+      $this->view('templates/header',$data);
+      $this->view('about/index', $data);
+      $this->view('templates/footer',$data);
+      
+   }
+   public function page()
+   {
+      $data['judul'] = 'Pages';
+      $this->view('templates/header',$data);
+      $this->view('about/page');
+      $this->view('templates/footer');
+   }
 
 }
 
