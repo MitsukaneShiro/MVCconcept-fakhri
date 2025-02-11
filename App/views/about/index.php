097ddd8e4b0cc5cbbc1ed4a45d3c352404235c8e
Cloud White Knight <muhammadzakywijaya01@gmail.com>
2025-02-11 11:04:07 +0800

    Update index.php

--- App/views/about/index.php
+++ App/views/about/index.php
@@ -1,4 +1,6 @@
+<div class="container">
+    <h1 class="mt-5">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/TB.JPG" alt="fakhri" width="220" class="rounded-circle shadow">
+    <p> halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['status']; ?>.</p>
 
-<h1>About Me</h1>
-<p> halo, nama saya <?= $data['nama']; ?>,
-umur saya <?= $data['umur']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
